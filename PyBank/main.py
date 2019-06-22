91e4694229ed3a1ced38ede7889470e2b7a33d51
LShafer <50753104+LShafer@users.noreply.github.com>
2019-06-22 12:43:11 -0500

    new updates on main

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,51 +1,31 @@
 import os
 import csv
-csvpath = os.path.join('..', 'Resources', 'budget_data.csv')
+csvpath = os.path.join('..', 'PyBank', 'budget_data.csv')
 
 with open(csvpath, newline='') as budget_data:
     csvreader = csv.reader(budget_data, delimiter=',')
     print(csvreader)
     csv_header = next(csvreader)
-    print(f"CSV Header: {csv_header}")
+    print(f'CSV Header: {csv_header}')
     for row in csvreader:
         print(row)
 
-    def count_rows(budget_data):
-        print("Total Months: " + len(budget_data))
+    count_rows = len(budget_data)
+    print("Total Months: " + count_rows)
 
 
     def net_total(budget_data):
-        print("Total: " + net_total)
-        for i = 0
-        total = 0
-        while(i < len(budget_data))
+        i = int(0)
+        total = int(0)
+        while i < len(budget_data):
             total = total + budget_data[i]
             i = i + 1
         print("Total: " + int(net_total))    
 
     def Average(budget_data):
-        for i = 0
-        total = 0
-        while(i < len(budget_data))
+        i = int(0)
+        total = int(0)
+        while i < len(budget_data):
             total = total + budget_data[i]
             i = i + 1
-        print("Average Change: " + int(total/len(budget_data)
-
-    def max(budget_data):
-        max = budget_data[0]
-        for item in budget_data:
-            if item > max:
-                max = item
-        print("Greatest Increase in Profits: " + "Date" + max
-
-
-    def min(budget_data):
-        min = budget_data[0]
-        for item in budget_data:
-            if item < min:
-                min = item
-        print("Greatest Decrease in Profits: " + "Date" + min)
-
-
-
-
+        print("Average Change: " + int(total/len(budget_data)))
