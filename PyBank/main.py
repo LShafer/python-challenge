5f968a2979ce71605a4d11e85d9a912236447c8d
LShafer <50753104+LShafer@users.noreply.github.com>
2019-06-23 21:52:43 -0500

    updates to main.py

--- PyBank/main.py
+++ PyBank/main.py
@@ -2,30 +2,62 @@ import os
 import csv
 csvpath = os.path.join('..', 'PyBank', 'budget_data.csv')
 
-with open(csvpath, newline='') as budget_data:
-    csvreader = csv.reader(budget_data, delimiter=',')
+file_output = "results.txt"
+
+with open(csvpath, 'r') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=',')
     print(csvreader)
-    csv_header = next(csvreader)
-    print(f'CSV Header: {csv_header}')
+    csv_header = next(csvreader, None)
+    print("Financial Analysis")
+    print("--------------------------------------")
+    
+    months = 0
+    month_count = []
+    total_profit = 0
+    initial_profit = 0
+    profit_change = []
+    
+    increase = ['', 9999999]
+    decrease = ['', 0]
+    
     for row in csvreader:
-        print(row)
+        months = months + 1
+        print(f"Total Months: {months}")
 
-    count_rows = len(budget_data)
-    print("Total Months: " + count_rows)
+        total_profit = total_profit + int(row[1])
+        print("Total Revenue: " + "$" + str(total_profit))
 
+        final_profit = int(row[1])
+        monthly_profits = final_profit - initial_profit
+        profit_change.append(monthly_profits)
+        average_profit = (monthly_profits/months)
+        print("Average Change: " + str(average_profit))
 
-    def net_total(budget_data):
-        i = int(0)
-        total = int(0)
-        while i < len(budget_data):
-            total = total + budget_data[i]
-            i = i + 1
-        print("Total: " + int(net_total))    
+        if monthly_profits > increase[1]:
+            increase[1] = monthly_profits
+            increase[0] = row[0]
+        else monthly_profits < decrease [1]:
+                decrease[1] = monthly_profits
+                decrease[0] = row[0]
+        print(f'Greatest Increase in Profits: {increase[0]} (${increase[1]}')
+        print(f'Greatest Decrease in Profits: {decrease[0]} (${decrease[1]}')
+        #profit = profit + [profit_change]
 
-    def Average(budget_data):
-        i = int(0)
-        total = int(0)
-        while i < len(budget_data):
-            total = total + budget_data[i]
-            i = i + 1
-        print("Average Change: " + int(total/len(budget_data)))
+        #for i in range(0,len(row[1]) -1):
+            #profit_change.append(int(final_profit[i+1]-int(initial_profit[i])))
+
+            
+    #for i[0]
+    #total = int(0)
+    #while i < len(budget_data):
+        #total = total + budget_data[i]
+        #i = i + 1
+    #print("Total: " + int(net_total))    
+
+#def Average(budget_data):
+    #i = int(0)
+    #total = int(0)
+    #while i < len(budget_data):
+        #total = total + budget_data[i]
+        #i = i + 1
+    #print("Average Change: " + int(total/len(budget_data)))
