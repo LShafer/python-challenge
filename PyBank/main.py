96600235997091989c582206d9bdfdbcf15e4b1b
LShafer <50753104+LShafer@users.noreply.github.com>
2019-06-24 15:13:30 -0500

    Update main.py

--- PyBank/main.py
+++ PyBank/main.py
@@ -23,32 +23,22 @@ with open(csvpath, newline='') as csvfile:
     
     for row in csvreader:
         months += 1
-        print(f"Total Months: {months}")
+        print(f'Total Months: {months}')
 
         total_profit += int(row[1])
-        #print("Total Revenue: " + "$" + str(total_profit))
+        print(f'Total Revenue: ${total_profit}')
 
         final_profit = int(row[1])
         monthly_profits = final_profit - initial_profit
         profit_change.append(monthly_profits)
-        average_profit = (monthly_profits/months)
-        #print("Average Change: " + "$" + str(average_profit))
+        average_profit = round(monthly_profits/months)
+        print(f'Average Change: ${average_profit}')
 
-        average = sum(profit_change)/ len(profit_change)
-        #print("new avg" + str(average))
-
-        highest = max(profit_change)
-        print("greatest increase" + str(highest))
-
-        lowest = min(profit_change)
-        print("decrease" +str(lowest))
-
-        #greatest_increase = int(row[1])
-        #if monthly_profits > greatest_increase:
-        #greatest_increase = monthly_profits
-        increase_month = row[0]
-        #else (monthly_profits < decrease[1]):
-            #decrease[1] = monthly_profits
-        decrease_month = row[0]
-        print(f'Greatest Increase in Profits: {increase_month} (${highest}')
-        print(f'Greatest Decrease in Profits: {decrease_month} (${lowest}')
+        if greatest_increase < int(row[1]):
+            greatest_increase = int(row[1])
+            increase_month = row[0]
+        if greatest_decrease > int(row[1]):
+            greatest_decrease = int(row[1])
+            decrease_month = row[0]
+        print(f'Greatest Increase in Profits: {increase_month} ${greatest_increase}')
+        print(f'Greatest Decrease in Profits: {decrease_month} ${greatest_decrease}')
