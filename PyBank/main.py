9da5f704294734baf9c96cb02e4fd43d0b01d4a4
LShafer <50753104+LShafer@users.noreply.github.com>
2019-06-24 14:57:46 -0500

    new updates main.py
    still working out/troubleshooting code.

--- PyBank/main.py
+++ PyBank/main.py
@@ -4,7 +4,7 @@ csvpath = os.path.join('..', 'PyBank', 'budget_data.csv')
 
 file_output = "results.txt"
 
-with open(csvpath, 'r') as csvfile:
+with open(csvpath, newline='') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
     print(csvreader)
     csv_header = next(csvreader, None)
@@ -16,28 +16,39 @@ with open(csvpath, 'r') as csvfile:
     total_profit = 0
     initial_profit = 0
     profit_change = []
-    
-    increase = ['', 9999999]
-    decrease = ['', 0]
+    greatest_increase = 0
+    increase_month = 0
+    greatest_decrease = 0
+    decrease_month = 0
     
     for row in csvreader:
-        months = months + 1
+        months += 1
         print(f"Total Months: {months}")
 
-        total_profit = total_profit + int(row[1])
-        print("Total Revenue: " + "$" + str(total_profit))
+        total_profit += int(row[1])
+        #print("Total Revenue: " + "$" + str(total_profit))
 
         final_profit = int(row[1])
         monthly_profits = final_profit - initial_profit
         profit_change.append(monthly_profits)
         average_profit = (monthly_profits/months)
-        print("Average Change: " + str(average_profit))
+        #print("Average Change: " + "$" + str(average_profit))
 
-        #if (monthly_profits > increase[1]):
-            #increase[1] = monthly_profits
-            #increase[0] = column[0]
+        average = sum(profit_change)/ len(profit_change)
+        #print("new avg" + str(average))
+
+        highest = max(profit_change)
+        print("greatest increase" + str(highest))
+
+        lowest = min(profit_change)
+        print("decrease" +str(lowest))
+
+        #greatest_increase = int(row[1])
+        #if monthly_profits > greatest_increase:
+        #greatest_increase = monthly_profits
+        increase_month = row[0]
         #else (monthly_profits < decrease[1]):
             #decrease[1] = monthly_profits
-            #decrease[0] = column[0]
-        #print(f'Greatest Increase in Profits: {increase[0]} (${increase[1]}')
-        #print(f'Greatest Decrease in Profits: {decrease[0]} (${decrease[1]}')
+        decrease_month = row[0]
+        print(f'Greatest Increase in Profits: {increase_month} (${highest}')
+        print(f'Greatest Decrease in Profits: {decrease_month} (${lowest}')
