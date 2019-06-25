dff5d76395f5961c035a1117d925b62c539244e8
LShafer <50753104+LShafer@users.noreply.github.com>
2019-06-24 23:37:08 -0500

    new updates main.py

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -6,7 +6,10 @@ total_votes = 0
 candidate_list = []
 percentage = 0
 candidate_votes = []
-winner = 0
+khan_votes = 0
+correy_votes = 0
+li_votes = 0
+otooley_votes = 0
 
 with open(csvpath, newline='') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
@@ -18,18 +21,37 @@ with open(csvpath, newline='') as csvfile:
     for row in csvreader:
         total_votes += 1
         print(f'Total Votes: {total_votes}')
-        print("------------------------")
+        #print("------------------------")
 
-    #list of candidate_list
-    #append.row[] // column 3
+    #list of candidates
+        #if row[2] not in candidate_list:
+            #candidate_list.append(row[2])
+        #print(f'{candidate_list}')
 
     #if candidate is on list then count until name changes
+        if (row[2] == "Khan"):
+            khan_votes += 1
+            #delete this print when % is done
+            print(f'Khan: {khan_votes}') 
+        #elif (row[2] == "Correy"):
+            #correy_votes += 1
+            #print(f'Correy: {correy_votes}')
+        #elif (row[2] == "Li"):
+            #li_votes += 1
+            #print(f'Li: {li_votes}')
+        #else (row[2] == "O'Tooley"):
+        #   otooley_votes += 1
+        #   print(f'O'Tooley: {otooley_votes}')   
     
     #percentage candidate/total_votes
-
+        khan_percentage = khan_votes/total_votes
+        print(f'Khan: {khan_percentage:.3%} ({khan_votes})')
+    
+    #print("------------------------------")
     #winner max(total votes)
+    #print("------------------------------")
 
-    #print 
+
 
     #output to election.txt
 
