6beee34a8d43172d9d71adaab889dff5c792d794
LShafer <50753104+LShafer@users.noreply.github.com>
2019-06-25 23:25:23 -0500

    moved print stmts to the bottom

--- PyBank/main.py
+++ PyBank/main.py
@@ -6,11 +6,9 @@ file_output = "results.txt"
 
 with open(csvpath, newline='') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
-    print(csvreader)
     csv_header = next(csvreader, None)
-    print("Financial Analysis")
-    print("--------------------------------------")
     
+    #define variables
     months = 0
     month_count = []
     total_profit = 0
@@ -22,24 +20,35 @@ with open(csvpath, newline='') as csvfile:
     decrease_month = 0
     
     for row in csvreader:
+        #count months
         months += 1
-        print(f'Total Months: {months}')
-
+       
+        #calculate total profit
         total_profit += int(row[1])
-        print(f'Total Revenue: ${total_profit}')
-
+        
+        #calculate profit change
         final_profit = int(row[1])
         monthly_profits = final_profit - initial_profit
         profit_change.append(monthly_profits)
         month_count.append(row[0])
         average_profit = round(monthly_profits/months)
-        print(f'Average Change: ${average_profit}')
-
+        
+        #calculate greatest increase/decrease
         if greatest_increase < int(row[1]):
             greatest_increase = int(row[1])
             increase_month = row[0]
         if greatest_decrease > int(row[1]):
             greatest_decrease = int(row[1])
             decrease_month = row[0]
-        print(f'Greatest Increase in Profits: {increase_month} ${greatest_increase}')
-        print(f'Greatest Decrease in Profits: {decrease_month} ${greatest_decrease}')
+
+    #print results
+    print("Financial Analysis")
+    print("--------------------------------------")   
+    print(f'Total Months: {months}')
+    print(f'Total Revenue: ${total_profit}')
+    print(f'Average Change: ${average_profit}')
+    print(f'Greatest Increase in Profits: {increase_month} ${greatest_increase}')
+    print(f'Greatest Decrease in Profits: {decrease_month} ${greatest_decrease}')
+    print("--------------------------------------")
+    
+    #output to txt
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -2,6 +2,7 @@ import os
 import csv
 csvpath = os.path.join('..', 'PyPoll', 'election_data.csv')
 
+#set variables
 total_votes = 0
 candidate_list = []
 percentage = 0
@@ -11,48 +12,74 @@ correy_votes = 0
 li_votes = 0
 otooley_votes = 0
 
+#open csv
 with open(csvpath, newline='') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
-    print(csvreader)
     csvheader = next(csvreader, None)
-    print("Election Results")
-    print("------------------------")
 
+    #calculate total votes
     for row in csvreader:
         total_votes += 1
-        print(f'Total Votes: {total_votes}')
-        #print("------------------------")
 
     #list of candidates
-        #if row[2] not in candidate_list:
-            #candidate_list.append(row[2])
-        #print(f'{candidate_list}')
+        if row[2] not in candidate_list:
+            candidate_list.append(row[2])
 
-    #if candidate is on list then count until name changes
+    #candidate votes
         if (row[2] == "Khan"):
             khan_votes += 1
-            #delete this print when % is done
-            print(f'Khan: {khan_votes}') 
-        #elif (row[2] == "Correy"):
-            #correy_votes += 1
-            #print(f'Correy: {correy_votes}')
-        #elif (row[2] == "Li"):
-            #li_votes += 1
-            #print(f'Li: {li_votes}')
-        #else (row[2] == "O'Tooley"):
-        #   otooley_votes += 1
-        #   print(f'O'Tooley: {otooley_votes}')   
-    
+        
+        elif (row[2] == "Correy"):
+            correy_votes += 1
+            
+        elif (row[2] == "Li"):
+            li_votes += 1
+            
+        elif (row[2] == "O'Tooley"):
+            otooley_votes += 1
+           
     #percentage candidate/total_votes
-        khan_percentage = khan_votes/total_votes
-        print(f'Khan: {khan_percentage:.3%} ({khan_votes})')
-    
-    #print("------------------------------")
-    #winner max(total votes)
-    #print("------------------------------")
+    khan_percentage = khan_votes/total_votes
+    correy_percentage = correy_votes/total_votes
+    li_percentage = li_votes/total_votes
+    otooley_percentage = otooley_votes/total_votes
+
+    #print results
+    print("Election Results")
+    print("------------------------")
+    print(f'Total Votes: {total_votes}')
+    print("------------------------")
+    print(f'Khan: {khan_percentage:.3%} ({khan_votes})')
+    print(f'Correy: {correy_percentage:.3%} ({correy_votes})')
+    print(f'Li: {li_percentage:.3%} ({li_votes})')
+    print(f'OTooley: {otooley_percentage:.3%} ({otooley_votes})')
+    print("------------------------------")
+
+    #determine winner 
+    winner = max(f'{khan_votes} {correy_votes} {li_votes} {otooley_votes}')
+    if winner == khan_votes:
+        print("Winner: " + "Khan")
+    elif winner == correy_votes:
+        print("Winner: " + "Correy")
+    elif winner == li_votes:
+        print("Winner: " + "Li")
+    elif winner == otooley_votes:
+        print("Winner: " + "O'Tooley")
+        print("------------------------------")
 
 
 
     #output to election.txt
+    file_output = "election.txt"
+
+    print("Election Results")
+    print("------------------------")
+    print(f'Total Votes: {total_votes}')
+    print("------------------------")
+    print(f'Khan: {khan_percentage:.3%} ({khan_votes})')
+    print(f'Correy: {correy_percentage:.3%} ({correy_votes})')
+    print(f'Li: {li_percentage:.3%} ({li_votes})')
+    print(f'OTooley: {otooley_percentage:.3%} ({otooley_votes})')
+    print("------------------------------")
 
 
